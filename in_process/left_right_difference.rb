e446e85137ea03a270bd9c2d34bc047d013ef494
Jim Riddle <jimriddle0@gmail.com>
2023-03-09 13:22:24 -0600

    working on left right diff

---
+++ in_process/left_right_difference.rb
@@ -0,0 +1,16 @@
+# @param {Integer[]} nums
+# @return {Integer[]}
+def left_rigth_difference(nums)
+    left_sum = [0];
+    right_sum = [0];
+    left_array = nums[1..-1]
+    right_nums = nums.reverse[1..-1]
+    require 'pry'; binding.pry
+    # if nums.length > 1
+    #     nums.each do |num|
+
+    #     end
+    # end
+end
+
+left_rigth_difference([10,4,8,3])
